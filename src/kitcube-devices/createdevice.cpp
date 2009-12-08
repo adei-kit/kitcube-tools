4995578ad340824dc493b2d6e2cdf369279ea93f
Norbert Flatinger <norbert.flatinger@kit.edu>
2009-12-08 16:56:46 +0100

    Added Norbert simulation device to build environment and to createdevice.cpp
    modified:   src/kitcube-devices/Makefile.am
    Added Norbert simulation device source files, so they get compiled.
    
    modified:   src/kitcube-devices/createdevice.cpp
    Norbert simulation device can be created now.

--- src/kitcube-devices/createdevice.cpp
+++ src/kitcube-devices/createdevice.cpp
@@ -7,18 +7,16 @@
  ***************************************************************************/
 
 
-
 #include "daqdevice.h"
 
 #include "mast.h"
 #include "ceilometer.h"
 #include "simrandom.h"
-
+#include "norbert.h"
 
 extern "C" void *createDevice(const char *devType) {
-	
 	if (strcasestr(devType, "Mast") > 0) return( new (Mast));
 	if (strcasestr(devType, "Ceilometer") > 0) return( new (Ceilometer));
 	if (strcasestr(devType, "SimRandom") > 0) return( new (SimRandom));
-		
+	if (strcasestr(devType, "Norbert") > 0) return( new (Norbert));
 }
