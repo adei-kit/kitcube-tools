73cd250962280d7b227837c46b8144e458f2b549
Norbert Flatinger <norbert.flatinger@kit.edu>
2010-06-11 18:04:58 +0200

    added readHeader and started readData

--- src/kitcube-devices/parsivel.cpp
+++ src/kitcube-devices/parsivel.cpp
@@ -22,9 +22,71 @@ parsivel::~parsivel(){
 
 
 void parsivel::readHeader(const char *filename){
-	printf("parsivel::readHeader: no header to read!\n");
+	char line[256];
+	
+	
+	if (debug >= 1)
+		printf("_____parsivel::readHeader(const char *filename)_____\n");
 	
 	lenHeader = 0;	// no header
+	noData = 9999999;
+	
+	// List of sensors
+	nSensors = 9;
+	if (sensor > 0 )
+		delete [] sensor;
+	sensor = new struct sensorType [nSensors];
+	
+	// set default value for height
+	for (int i = 0; i < nSensors; i++) {
+		sensor[i].height = 0;
+	}
+	
+	sensor[0].comment = "Weather code";
+	sensor[0].data_format = "<scalar>";
+	
+	sensor[1].comment = "Accumulated rain amount (mm)";
+	sensor[1].data_format = "<scalar>";
+	
+	sensor[2].comment = "Rain intensity (mm/h)";
+	sensor[2].data_format = "<scalar>";
+	
+	sensor[3].comment = "Radar reflectivity (dBZ)";
+	sensor[3].data_format = "<scalar>";
+	
+	sensor[4].comment = "Sichtweite";
+	sensor[4].data_format = "<scalar>";
+	
+	sensor[5].comment = "Signal amplitude laser";
+	sensor[5].data_format = "<scalar>";
+	
+	sensor[6].comment = "drop velocities (m/s)";
+	sensor[6].data_format = "<profile size=\"32\"> <unknown unit=\"no unit\">";
+	for (int i = 1; i < 32; i++) {
+		sprintf(line, "%i ", i);
+		sensor[0].data_format += line;
+	}
+	sensor[6].data_format += "32</unknown> </profile>";
+	sensor[6].type = "profile";
+	
+	sensor[7].comment = "drop density (1/m^3/mm)";
+	sensor[7].data_format = "<profile size=\"32\"> <unknown unit=\"no unit\">";
+	for (int i = 1; i < 32; i++) {
+		sprintf(line, "%i ", i);
+		sensor[0].data_format += line;
+	}
+	sensor[7].data_format += "32</unknown> </profile>";
+	sensor[7].type = "profile";
+	
+	sensor[8].comment = "Number of drops";
+	sensor[8].data_format = "<2D size=\"32*32\">";
+	sensor[8].type = "profile";
+	
+	if (debug >= 1) {
+		for (int i = 0; i < nSensors; i++) {
+			printf("Sensor %3d: %s\n", i + 1, sensor[i].comment.c_str());
+		}
+	}
 }
 
 
@@ -100,5 +162,98 @@ unsigned int parsivel::getSensorGroup(){
 
 
 void parsivel::readData(const char *dir, const char *filename){
-	fd_eof = false;
+	std::string filenameData;
+	float* local_sensorValue;
+	unsigned long lastPos;
+	struct timeval lastTime;
+	char line[4150];
+	FILE *fmark;
+	unsigned long lastIndex;
+	struct timeval timestamp_data;
+	int iLoop;
+	char *lPtr, *date_time;
+	
+	
+	if (debug >= 1)
+		printf("_____parsivel::readData(const char *dir, const char *filename)_____\n");
+	
+	// Compile file name
+	filenameData = dir;
+	filenameData += filename;
+	
+	// If number of sensors is unknown read the header first
+	if (nSensors == 0)
+		readHeader(filenameData.c_str());
+	if (sensor[0].name.length() == 0)
+		getSensorNames(sensorListfile.c_str());
+	
+#ifdef USE_MYSQL
+	if (db == 0) {
+		openDatabase();
+	} else {
+		// Automatic reconnect
+		if (mysql_ping(db) != 0){
+			printf("Error: Lost connection to database - automatic reconnect failed\n");
+			throw std::invalid_argument("Database unavailable\n");
+		}
+	}
+#endif
+	
+	// number of sensors here: 6 + 32 + 32 + 32 * 32
+	local_sensorValue = new float [nSensors - 3 + 32 + 32 + 32 * 32];
+	
+	if (debug > 3) printf("Open data file %s\n", filenameData.c_str());
+	fdata = fopen(filenameData.c_str(), "r");
+	if (fdata <= 0) {
+		printf("Error opening data file %s\n", filenameData.c_str());
+		return;
+	}
+	
+	// Get the last time stamp + file pointer from 
+	lastPos = 0;
+	lastTime.tv_sec = 0;
+	lastTime.tv_usec = 0;
+	
+	sprintf(line, "%s.kitcube-reader.marker.%03d.%d", dir, moduleNumber, sensorGroupNumber);
+	filenameMarker = line;
+	if (debug > 3) printf("Get marker from %s\n", filenameMarker.c_str());
+	fmark = fopen(filenameMarker.c_str(), "r");
+	if (fmark > 0) {
+		fscanf(fmark, "%ld %ld %ld %ld", &lastIndex,  &lastTime.tv_sec, &lastTime.tv_usec, &lastPos);
+		fclose(fmark);
+		
+		// Read back the data time stamp of the last call
+		timestamp_data.tv_sec = lastTime.tv_sec;
+		timestamp_data.tv_usec = lastTime.tv_usec;
+		
+		if (debug > 4) printf("Last time stamp was %ld\n", lastTime.tv_sec);
+	}
+	
+	if (lastPos == 0)
+		lastPos = lenHeader; // Move to the the first data
+	
+	// Find the beginning of the new data
+	if (debug > 4)
+		printf("LastPos: %ld\n", lastPos);
+	fseek(fdata, lastPos, SEEK_SET);
+	
+	iLoop = 0;
+	lPtr = (char *) 1;
+	while ((lPtr > 0) && (iLoop < 100)) {
+		lPtr = fgets(line, 4150, fdata);
+		
+		if (lPtr != NULL) {
+			if (strlen(line) != 24) {
+				// bail out
+			} else{
+				// read date and time
+				date_time = strtok(line, ",");
+				date_time = strtok(NULL, ",");
+			}
+			
+		}
+		
+		
+		
+	}
 }
